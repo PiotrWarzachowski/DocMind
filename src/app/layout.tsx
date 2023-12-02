1368d58edb0effcbaa1a68666141f4aa520fdad2
Piotr Warząchowski <108893195+PiotrWarzachowski@users.noreply.github.com>
2023-12-02 16:05:50 +0100

    Frontend: add home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='light'>
+      <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,17 @@
+import {cn} from '@/lib/utils'
+const MaxWidthWrapper = ({
+    className,
+    children
+}: {
+    className?: string
+    children: React.ReactNode
+}) => {
+
+  return (
+    <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20" , className)}>
+        {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
